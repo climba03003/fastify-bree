50e7df28dd52592c0f9546f576ee00a3fa492620
KaKa <climba03003@gmail.com>
2021-12-03 15:10:25 +0800

    feat: use plugin (#16)
    * feat: use plugin
    
    * fix: worker script
    
    * fix: remove node prefix

---
+++ lib/plugin.ts
@@ -0,0 +1,42 @@
+import { Worker } from 'worker_threads'
+
+export function BreeTS (options: any, Bree: any): void {
+  options = options ?? {}
+
+  const oldInit = Bree.prototype.init
+
+  // we update init script, so the options is match for TypeScript
+  Bree.prototype.init = function () {
+    // add `.ts` extension to supported list
+    if (this.config.acceptedExtensions.includes('.ts') === false) {
+      this.config.acceptedExtensions.push('.ts')
+    }
+
+    // if it use default option, we change it to `ts`
+    if (this.config.defaultExtension === 'js') {
+      this.config.defaultExtension = 'ts'
+    }
+
+    oldInit.bind(this)()
+  }
+
+  const oldCreateWorker = Bree.prototype.createWorker
+
+  // we update the create worker function
+  // so it use the correct worker
+  Bree.prototype.createWorker = function (filename: string, options: any) {
+    if (filename.endsWith('.ts')) {
+      options.eval = true
+      options.workerData.__filename = filename
+      options.workerData.__tsNodeOptions = options.tsNodeOptions
+
+      return new Worker(`
+const workerData = require('worker_threads').workerData
+require('ts-node').register(workerData.__tsNodeOptions)
+require(workerData.__filename)
+      `, options)
+    }
+
+    return oldCreateWorker(filename, options)
+  }
+}
